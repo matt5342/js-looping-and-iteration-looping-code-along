0057d62d28ef76cce44faf8d222ba8bcf69c4824
Matt <mattfsewell@gmail.com>
2021-02-20 14:34:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(array){
+    let newarray = [];
+    for (let i = 0; i < array.length; i++){
+        newarray.push(`Thank you, ${array[i]}, for the wonderful surprise gift!`);
+    }
+    return newarray;
+}
+function countDown(num){
+    let i = num
+    while(i >= 0){
+        console.log(i);
+        i--;
+    }
+}
